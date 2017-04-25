f00443105653a82fa3cd10f32d3511a37b8a0850
jeanron100 <yangjianrong1985@163.com>
2017-04-25 10:42:15 +0800

    check node status
    check node status, need one parameter with node name which included in init.lst

---
+++ check_node.sh
@@ -0,0 +1,14 @@
+new_node_name=$1
+if [[ -z ${new_node_name} ]]; then
+ echo Node Name is needed ~~~
+ exit
+fi
+node_port=`cat init.lst|grep -w ${new_node_name}|awk '{print $1}'`
+node_name=`cat init.lst|grep -w ${new_node_name}|awk '{print $2}'`
+echo ${node_port}
+if [[ ${node_name} = ${new_node_name} ]];then
+mysql -P${node_port} -S /home/data/${node_name}/${node_name}.sock -e "select uuid(); select *from performance_schema.replication_group_members;"
+mysql -P${node_port} -S /home/data/${node_name}/${node_name}.sock -e " select *from performance_schema. replication_group_member_stats \G"
+else
+ echo Node Name not found in init.lst ~~~ 
+fi
