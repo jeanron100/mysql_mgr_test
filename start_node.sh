333f2022d913a173d280141bd5a1b135e7706b9e
jeanron100 <yangjianrong1985@163.com>
2017-08-30 17:02:26 +0800

    start dedicated node

---
+++ start_node.sh
@@ -0,0 +1,14 @@
+
+new_node_name=$1
+. ./auto.cnf
+
+if [[ -z ${new_node_name} ]]; then
+ echo Node Name is needed ~~~
+ exit
+fi
+node_name=`cat /U01/soft1/soft/mgr_install/init.lst|grep -w ${new_node_name}|awk '{print $2}'`
+if [[ ${node_name} = ${new_node_name} ]];then
+${base_dir}/bin/mysqld_safe --defaults-file=${base_data_dir}/${node_name}/${node_name}.cnf &
+else
+ echo Node Name not found in init.lst ~~~ 
+fi
