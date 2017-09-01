888a2580c027a3fa9cabc490a7c29f629cadb245
jeanron100 <yangjianrong1985@163.com>
2017-09-01 16:45:22 +0800

    Update stop_node.sh

--- stop_node.sh
+++ stop_node.sh
@@ -6,8 +6,8 @@ if [[ -z ${new_node_name} ]]; then
  echo Node Name is needed ~~~
  exit
 fi
-node_port=`cat /U01/soft1/soft/mgr_install/init.lst|grep -w ${new_node_name}|awk '{print $1}'`
-node_name=`cat /U01/soft1/soft/mgr_install/init.lst|grep -w ${new_node_name}|awk '{print $2}'`
+node_port=`cat init.lst|grep -w ${new_node_name}|awk '{print $1}'`
+node_name=`cat init.lst|grep -w ${new_node_name}|awk '{print $2}'`
 echo ${node_port}
 if [[ ${node_name} = ${new_node_name} ]];then
 ${base_dir}/bin/mysqladmin -P${node_port} -S ${base_data_dir}/${node_name}/${node_name}.sock shutdown 
