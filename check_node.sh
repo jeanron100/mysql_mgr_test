e64af280a8b49e694569cc705ed8fda3ee248ed0
jeanron100 <yangjianrong1985@163.com>
2018-12-16 17:46:53 +0800

    Update check_node.sh

--- check_node.sh
+++ check_node.sh
@@ -10,7 +10,7 @@ node_port=`cat init.lst|grep -w ${new_node_name}|awk '{print $1}'`
 node_name=`cat init.lst|grep -w ${new_node_name}|awk '{print $2}'`
 echo ${node_port}
 if [[ ${node_name} = ${new_node_name} ]];then
-${base_dir}/bin/mysql -P${node_port} -S ${base_data_dir}${node_name}/${node_name}.sock -e "select uuid(); select *from performance_schema.replication_group_members;"
+${base_dir}/bin/mysql -P${node_port} -S ${base_data_dir}/${node_name}/${node_name}.sock -e "select uuid(); select *from performance_schema.replication_group_members;"
 ${base_dir}/bin/mysql -P${node_port} -S ${base_data_dir}/${node_name}/${node_name}.sock -e " select *from performance_schema. replication_group_member_stats \G"
 else
  echo Node Name not found in init.lst ~~~ 
